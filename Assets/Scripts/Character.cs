31bc2ccee4e7952e3e913c5e1ddde60e5d103177
Devin <devinpquinn4@gmail.com>
2019-11-18 23:29:15 -0500

    Script setup

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Basic Info")]
+    public GameManager gm;
+    public int id;
+    public string myName;
+    public int gender;
+    public int[] regards;
+    bool eliminated = false;
+
+    [Header("Voting Info")]
+    private int myVote;
+    private int voteStrength;
+}
