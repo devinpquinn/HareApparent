456462a6701e0e6ff78da5a5ef0111c393e7add9
Devin <devinpquinn4@gmail.com>
2019-11-19 12:17:43 -0500

    Moar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,11 +18,30 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void ResetVotes() // sets each character's first vote choice at each round start
+    {
+        foreach (NPC currentChar in characters)
+        {
+            
+        }
+    }
+
     public void CalculateVotes() // precalculates NPCs' preferred moves before player phase
     {
         foreach(NPC currentChar in characters)
         {
-
+            int thisID = currentChar.id;
+            foreach(NPC partner in characters)
+            {
+                int partnerID = partner.id;
+                foreach(Character target in characters)
+                {
+                    if(target.id != thisID && target.id != partnerID)
+                    {
+                        partner.CompareVote(partner.regards[currentChar.id], partner.regards[target.id], target.id);
+                    }
+                }
+            }
         }
     }
 
