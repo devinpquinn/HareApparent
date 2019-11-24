76afa80000a7c092dd23128fd10cb01adae10966
Devin <devinpquinn4@gmail.com>
2019-11-23 22:23:42 -0500

    Beginning RPGTalk setup

---
+++ Assets/Scripts/NPCTalk.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalk : MonoBehaviour
+{
+    public RPGTalk myTalk;
+    public string myName;
+}
